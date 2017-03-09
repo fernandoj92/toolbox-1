ea782f372ce7dea432963d234294bc8614f35839
fernandoj92 <fernandoj92@gmail.com>
2017-03-09 15:24:16 +0100

    testVMPBug update

--- examples/src/main/java/eu/amidst/ferjorosa/examples/TestVMPBug.java
+++ examples/src/main/java/eu/amidst/ferjorosa/examples/TestVMPBug.java
@@ -1,14 +1,17 @@
 package eu.amidst.ferjorosa.examples;
 
-import eu.amidst.core.datastream.DataInstance;
-import eu.amidst.core.datastream.DataOnMemory;
-import eu.amidst.core.datastream.DataStream;
+import eu.amidst.core.datastream.*;
 import eu.amidst.core.io.DataStreamLoader;
 import eu.amidst.core.learning.parametric.ParallelMaximumLikelihood;
 import eu.amidst.core.learning.parametric.bayesian.SVB;
 import eu.amidst.core.models.BayesianNetwork;
 import eu.amidst.core.models.DAG;
+import eu.amidst.core.utils.DAGGenerator;
+import eu.amidst.core.variables.Variable;
 import eu.amidst.core.variables.Variables;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by equipo on 09/03/2017.
@@ -16,20 +19,80 @@ import eu.amidst.core.variables.Variables;
 public class TestVMPBug {
 
     public static void main(String[] args) throws Exception {
-
-        DataStream<DataInstance> dataSprinkler = DataStreamLoader.open("datasets/ferjorosa/sprinklerData300.arff");
-        DataStream<DataInstance> dataSprinklerHiddenCloudy = DataStreamLoader.open("datasets/ferjorosa/sprinklerDataHidden.arff");
-        DataStream<DataInstance> dataAsia = DataStreamLoader.open("datasets/ferjorosa/Asia_train.arff");
-        DataStream<DataInstance> dataAsiaHiddenVisit = DataStreamLoader.open("datasets/ferjorosa/sprinklerData300.arff");
-        DataStream<DataInstance> dataAsiaHiddenVisitSmoking = DataStreamLoader.open("datasets/ferjorosa/sprinklerData300.arff");
-
+        //asiaHiddenVisitComparison();
+        //asiaLCM();
+        sprinklerHiddenCloudyComparison();
+        //sprinklerLCM();
     }
 
-    /**
-     * Creamos la estructura de la red bayesiana Asia tradicional sin variables latentes (ocultas).
-     *
-     * @return la red bayesiana Asia sin variables latentes.
-     */
+    /******************************************************************************************************************/
+    /******************************************************************************************************************/
+    /******************************************************************************************************************/
+
+    // Este caso compara la red de Asia tradicional en la que el nodo vVisitAsia se oculta (LV)
+    private static void asiaHiddenVisitComparison(){
+        System.out.println("\n\n/*******************************************************************************/");
+        System.out.println("/******************************** ASIA MLE *************************************/");
+        System.out.println("/*******************************************************************************/");
+        DataStream<DataInstance> dataAsia = DataStreamLoader.open("datasets/ferjorosa/data/Asia_train.arff");
+        DAG asiaDAG = defineAsiaDAG(dataAsia);
+        BayesianNetwork asiaMLE = learnWithMLE(asiaDAG, dataAsia);
+        System.out.println(asiaMLE.toString());
+
+        System.out.println("\n\n/*******************************************************************************/");
+        System.out.println("/******************************** ASIA SVB *************************************/");
+        System.out.println("/*******************************************************************************/");
+        DataStream<DataInstance> dataAsiaHiddenVisit = DataStreamLoader.open("datasets/ferjorosa/data/Asia_trainHidden.arff");
+        DAG asiaDAGHiddenVisit = defineAsiaDAGHiddenVisit(dataAsiaHiddenVisit);
+        BayesianNetwork asiaSVB = learnWithSVB(asiaDAGHiddenVisit, dataAsiaHiddenVisit);
+        System.out.println(asiaSVB.toString());
+    }
+
+    // Este caso aprende un LCM con todas las variables de Asia y poniendo un root oculto
+    private static void asiaLCM(){
+        System.out.println("\n\n/*******************************************************************************/");
+        System.out.println("/******************************** ASIA LCM *************************************/");
+        System.out.println("/*******************************************************************************/");
+        DataStream<DataInstance> dataAsiaLCM = DataStreamLoader.open("datasets/ferjorosa/data/Asia_train.arff");
+        DAG asiaLCM = defineAsiaDAGLCM(dataAsiaLCM);
+        BayesianNetwork asiaLCMSVB = learnWithSVB(asiaLCM, dataAsiaLCM);
+        System.out.println(asiaLCMSVB.toString());
+    }
+
+    private static void sprinklerHiddenCloudyComparison(){
+
+        System.out.println("\n\n/*******************************************************************************/");
+        System.out.println("/******************************** Sprinkler MLE *************************************/");
+        System.out.println("/*******************************************************************************/");
+        DataStream<DataInstance> dataSprinkler = DataStreamLoader.open("datasets/ferjorosa/data/sprinklerData300.arff");
+        DAG sprinklerDAG = defineSprinklerDAG(dataSprinkler);
+        BayesianNetwork sprinklerMLE = learnWithMLE(sprinklerDAG, dataSprinkler);
+        System.out.println(sprinklerMLE.toString());
+
+        System.out.println("\n\n/*******************************************************************************/");
+        System.out.println("/******************************** Sprinkler SVB *************************************/");
+        System.out.println("/*******************************************************************************/");
+        DataStream<DataInstance> dataHiddenCloudy = DataStreamLoader.open("datasets/ferjorosa/data/sprinklerDataHidden.arff");
+        DAG sprinklerDAGHiddenCloudy = defineSprinklerDAGHiddenCloudy(dataHiddenCloudy);
+        BayesianNetwork sprinklerSVB = learnWithSVB(sprinklerDAGHiddenCloudy, dataHiddenCloudy);
+        System.out.println(sprinklerSVB.toString());
+    }
+
+    private static void sprinklerLCM(){
+        System.out.println("\n\n/*******************************************************************************/");
+        System.out.println("/******************************** Sprinkler LCM *************************************/");
+        System.out.println("/*******************************************************************************/");
+        DataStream<DataInstance> dataSprinklerLCM = DataStreamLoader.open("datasets/ferjorosa/data/sprinklerData300.arff");
+        DAG sprinklerLCM = defineSprinklerLCM(dataSprinklerLCM);
+        BayesianNetwork sprinklerLCMSVB = learnWithSVB(sprinklerLCM, dataSprinklerLCM);
+        System.out.println(sprinklerLCMSVB.toString());
+    }
+
+    /******************************************************************************************************************/
+    /******************************************************************************************************************/
+    /******************************************************************************************************************/
+
+    // Creamos el DAG tradicional de Asia sin variables latentes (ocultas).
     private static DAG defineAsiaDAG(DataStream<DataInstance> data){
 
         Variables variables = new Variables(data.getAttributes());
@@ -48,11 +111,46 @@ public class TestVMPBug {
         return dag;
     }
 
-    /**
-     * Creamos la estructura de la red bayesiana "Sprinkler" tradicional sin variables latentes (ocultas)
-     *
-     * @return la red bayesiana sprinkler sin variables latentes.
-     */
+    // Creamos el DAG tradicional de Asia con vVisitAsia como variable latente.
+    private static DAG defineAsiaDAGHiddenVisit(DataStream<DataInstance> data){
+
+        List<Attribute> attributes = new ArrayList<>();
+        attributes.add(data.getAttributes().getAttributeByName("vTuberculosis"));
+        attributes.add(data.getAttributes().getAttributeByName("vSmoking"));
+        attributes.add(data.getAttributes().getAttributeByName("vLungCancer"));
+        attributes.add(data.getAttributes().getAttributeByName("vTbOrCa"));
+        attributes.add(data.getAttributes().getAttributeByName("vXRay"));
+        attributes.add(data.getAttributes().getAttributeByName("vBronchitis"));
+        attributes.add(data.getAttributes().getAttributeByName("vDyspnea"));
+
+        Attributes attributeCollection = new Attributes(attributes);
+
+        Variables variables = new Variables(attributeCollection);
+
+        Variable hiddenVisitAsia = variables.newMultinomialVariable("hiddenVisitAsia", 2);
+
+        DAG dag = new DAG(variables);
+
+        dag.getParentSet(variables.getVariableByName("vXRay")).addParent(variables.getVariableByName("vTbOrCa"));
+        dag.getParentSet(variables.getVariableByName("vDyspnea")).addParent(variables.getVariableByName("vTbOrCa"));
+        dag.getParentSet(variables.getVariableByName("vDyspnea")).addParent(variables.getVariableByName("vBronchitis"));
+        dag.getParentSet(variables.getVariableByName("vTbOrCa")).addParent(variables.getVariableByName("vTuberculosis"));
+        dag.getParentSet(variables.getVariableByName("vTbOrCa")).addParent(variables.getVariableByName("vLungCancer"));
+        dag.getParentSet(variables.getVariableByName("vTuberculosis")).addParent(hiddenVisitAsia);
+        dag.getParentSet(variables.getVariableByName("vLungCancer")).addParent(variables.getVariableByName("vSmoking"));
+        dag.getParentSet(variables.getVariableByName("vBronchitis")).addParent(variables.getVariableByName("vSmoking"));
+
+        return dag;
+    }
+
+    // Creamos un LCM con todas los atributos de Asia y una LV como raiz
+    private static DAG defineAsiaDAGLCM(DataStream<DataInstance> data){
+        return  DAGGenerator.getHiddenNaiveBayesStructure(data.getAttributes(),"GlobalHidden", 3);
+    }
+
+    /******************************************************************************************************************/
+
+    // Creamos la estructura de la red bayesiana "Sprinkler" tradicional sin variables latentes (ocultas)
     private static DAG defineSprinklerDAG(DataStream<DataInstance> data){
 
         Variables variables = new Variables(data.getAttributes());
@@ -66,6 +164,32 @@ public class TestVMPBug {
 
         return dag;
     }
+
+    // Creamos la estructura de la red bayesiana Sprinkler tradicional con cloudy como LV
+    private static DAG defineSprinklerDAGHiddenCloudy(DataStream<DataInstance> data){
+
+        Variables variables = new Variables(data.getAttributes());
+
+        Variable latentCloudy = variables.newMultinomialVariable("latentCloudy", 2);
+
+        DAG dag = new DAG(variables);
+
+        dag.getParentSet(variables.getVariableByName("sprinkler")).addParent(latentCloudy);
+        dag.getParentSet(variables.getVariableByName("rain")).addParent(latentCloudy);
+        dag.getParentSet(variables.getVariableByName("wetGrass")).addParent(variables.getVariableByName("sprinkler"));
+        dag.getParentSet(variables.getVariableByName("wetGrass")).addParent(variables.getVariableByName("rain"));
+
+        return dag;
+    }
+
+    // Creamos un LCM con todos los atributos de Sprinkler y una LV como raiz
+    private static DAG defineSprinklerLCM(DataStream<DataInstance> data){
+        return  DAGGenerator.getHiddenNaiveBayesStructure(data.getAttributes(),"GlobalHidden", 2);
+    }
+
+    /******************************************************************************************************************/
+    /******************************************************************************************************************/
+    /******************************************************************************************************************/
 
     /**
      * Aprende los parametros de la red utilizando MLE. Con cada llamada se crea una nueva instancia, asi que no hay
@@ -109,7 +233,7 @@ public class TestVMPBug {
         svb.initLearning();
 
         //Then we show how we can perform parameter learnig by a sequential updating of data batches.
-        for (DataOnMemory<DataInstance> batch : data.iterableOverBatches(1000)){
+        for (DataOnMemory<DataInstance> batch : data.iterableOverBatches(100)){
             svb.updateModel(batch);
         }
 
